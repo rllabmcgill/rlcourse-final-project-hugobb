f2b2ebfb46b2840ebe125976d7e05830a7b8a607
Tom Bosc <bosc.tom@gmail.com>
2017-04-18 17:39:44 -0400

    multiagent new env with cooperation seems to work

--- tests/test_multiagent_coop.py
+++ tests/test_multiagent_coop.py
@@ -2,24 +2,42 @@
 
 import numpy as np
 
-from env.multiagent_coop import MultiAgentCoop
+from lib.env.multiagent_coop import MultiAgentCoop
 from lib.rpg_agent.random_agent import RandomAgent
 
-n_agents = 2
-n_landmarks = 2
-grid_size = (2,2)
+n_agents = 4
+n_landmarks = 4
+grid_size = (5,5)
+V = 5
 
-env = MultiAgentCoop(n_agents, n_landmarks, grid_size)
+env = MultiAgentCoop(n_agents, n_landmarks, grid_size, V)
+obs_size = env.observation_space_size()
+# obs space should be: agent id + position of itself + pos of all landmarks + goal
+# where each pos is 2 values and goal is 2 values (agent id, landmark id)
+
+print "env observation space size:", obs_size
+assert(obs_size == n_agents + 2 + n_landmarks * 2 + n_agents + n_landmarks)
+
+print "env action space size:", env.action_space_size()
 agents = [RandomAgent(env.action_space_size()) for _ in range(n_agents)]
+env.render()
 
-done = [False for i in range(n_agents)]
-for t in range(1000):
-    actions = [a.sample_action(None, None) for a in agents]
-    o, r, done, _ = env.step(actions)
-    print o
-    print r
-    print done
-    if np.all(done):
-        break
+for j in range(20):
+    env.reset()
+    cum_return = 0
+
+    for t in range(100):
+        actions = [a.sample_action(None, None) for a in agents]
+        speeches = [np.random.choice(V) for _ in agents]
+        o, r, done, _ = env.step(actions, speeches)
+        if j==t:
+            print o
+        assert(len(set(r)) == 1) # all rewards are shared by agents
+        assert(len(set(done)) == 1) # episode ends at the same time for all agents
+        cum_return += r[0]
+        #env.render()
+        if np.all(done):
+            break
+    assert(cum_return == n_agents or not np.all(done) and cum_return < n_agents)
 print done
 print "stopped at", t
